1cef8c7fb66220fc5bb0a4fe9b01f823a52de822
Matheus7lima <31293651+Matheus7lima@users.noreply.github.com>
2017-09-05 21:24:18 -0300

    Add files via upload

---
+++ projetoOBJS/JS/JS.js
@@ -0,0 +1,18 @@
+var obj = new Object();
+obj["nome"] = "matheus";
+alert(obj.nome);
+alert("ferreira")
+
+class User {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+var user = new User("Matheus", "?");
+alert(user.name);
+alert(user.age);
+
+class Pessoa(){
+    
+}
---
+++ projetoOBJS/JS/pessoa.js
@@ -0,0 +1,31 @@
+class Pessoa{
+   constructor(name, nascimento, rg){
+       this.name = name;
+       this.nascimento = data;
+       this.rg = rg;
+   }
+    
+Eat(){
+   
+}
+    
+    
+Falar(){
+   
+}
+   class data {
+       constructor(dia, mes ano){
+           this.dia = dia;
+           this.mes = mes;
+           this.ano = ano;
+       }
+   }
+
+    class Aluno extends Pessoa{
+        constructor(matric){
+            this.matric=matric;
+        }
+    }
+
+var nascimento = new
+}
